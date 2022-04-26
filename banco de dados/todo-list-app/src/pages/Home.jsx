e70852fececf5e3d7e759ed0f2a71690b3713218
Falller <marlon.emanutec@gmail.com>
2022-04-25 21:54:38 -0300

    pronto

--- banco de dados/todo-list-app/src/pages/Home.jsx
+++ banco de dados/todo-list-app/src/pages/Home.jsx
@@ -1,32 +1,37 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { TodoListItem } from '../components/TodoListItem';
 import './Home.css';
 
 export function Home () {
-    const itemsArray = [
-        {id: 1, text:"Acordar as 7h", checked: false},
-        {id: 2, text:"Almoçar as 12h", checked: false},
-        {id: 3, text:"Dormir as 22h", checked: false}
-    ];
-    const itemsArray2 = [
-        {id: 4, text:"Acordar as 7h", checked: false},
-        {id: 5, text:"Almoçar as 12h", checked: false},
-        {id: 6, text:"Dormir as 22h", checked: false}
-    ];
+    const [itemsArray, setitemsArray] = useState(() => 
+    {
+        let array = [
+            {id: 1, text:"Acordar as 7h", checked: false},
+            {id: 2, text:"Almoçar as 12h", checked: false},
+            {id: 3, text:"Dormir as 22h", checked: false}
+        ]
+        const storedArray = localStorage.getItem("itemsArray")
+        console.log(localStorage.getItem("itemsArray"))
 
-//    const [itemsArrayState, setitemsArrayState] = useState(itemsArray);
-console.log("array sem mudança",itemsArray);
+        if(storedArray != null) {
+            array = JSON.parse(storedArray)
+        } else {
+            const itemsArrayString = JSON.stringify(array)
+            localStorage.setItem("itemsArray", itemsArrayString)
+        }
+        return array;
+    })
+
+    console.log("array sem mudança",itemsArray);
+
 
     function changeArray (id, bool) {
         const index = itemsArray.findIndex((itemsArray) => itemsArray.id == id)
         console.log("index", index);
         itemsArray[index].checked = bool
-        console.log("aqui o array deve estar mudado",itemsArray)
-        
-    }
-    
-localStorage.setItem("abc","seu item");
-sessionStorage.setItem("def", "teste")
+        console.log("aqui o array deve estar mudado",itemsArray)   
+        localStorage.setItem("itemsArray", JSON.stringify(itemsArray))   
+    };
 
     return (
         <div className="container">
@@ -35,11 +40,27 @@ sessionStorage.setItem("def", "teste")
                 <TodoListItem key={id} id={id} text={text} checked={checked} changeArray={changeArray}/>
                 )
             })}
-            <hr />
-            {itemsArray2.map((obj) => {
-                return <TodoListItem key={obj.id}{...obj} />
-            })}
         </div>
     )
-    
 }
+
+/**function addItemTodoList (id, checked) {
+    const ultimoId = itemsArray[itemsArray.length - 1].id
+    setitemsArray([...itemsArray, {
+        id: ultimoId + 1, 
+        text: "Qualquer tarefa importante", 
+        checked: false
+    }])
+}*/
+/**useEffect(() => {
+    const storedArray = localStorage.getItem("itemsArray");
+    console.log(localStorage.getItem("itemsArray"));
+    if(storedArray != null) {
+        array = JSON.parse(storedArray)
+        setitemsArray(array);
+    } else {
+        const itemsArrayString = JSON.stringify(itemsArray);
+        localStorage.setItem("itemsArray", itemsArrayString)
+    }
+}, [])
+*/
